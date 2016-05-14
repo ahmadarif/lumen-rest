fd3065766a755999644c2b6756940a013e500da6
Ahmad Arif <ahmad.arif019@gmail.com>
2016-05-14 08:11:34 +0700

    plus ultra commit

---
+++ protected/app/Http/routes.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+// $app->get('/', function() use ($app) {
+// 	return $app->version();
+// });
+
+$app->group(['prefix' => 'api/v1', 'namespace' => 'App\Http\Controllers'], function($app) {
+	$app->get('article', 'ArticleController@index');
+	$app->get('article/{id}', 'ArticleController@getArticle');
+	$app->post('article', 'ArticleController@saveArticle');
+	$app->put('article/{id}', 'ArticleController@updateArticle');	
+	$app->delete('article/{id}', 'ArticleController@deleteArticle');
+});
